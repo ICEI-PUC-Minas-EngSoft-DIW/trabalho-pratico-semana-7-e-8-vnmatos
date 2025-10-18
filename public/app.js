268ad196f64a5bd3f799da58b1c4c362d9bd7256
VInicius Matos <viniciustrabalhos76@gmail.com>
2025-10-18 16:19:40 -0300

    ajustei o JS para quando clicar nos icones, redirecionar para a pagina de detalhes específica

--- public/app.js
+++ public/app.js
@@ -4,40 +4,46 @@ const destinos = [
         pais: "Itália",
         imagem: "images/cards/milao.webp",
         descricao: "A capital da moda, do design e da economia da Itália",
+        link: "milao.html"
     },
-
     {
         nome: "Paris",
         pais: "França",
         descricao: "A cidade luz, famosa pela Torre Eiffel e pela culinária.",
         imagem: "images/cards/paris.jpg",
+        link: "paris.html"
     },
-
     {
-        nome:"Londres",
+        nome: "Londres",
         pais: "Inglaterra",
         descricao : "Uma metrópole global, centro financeiro e cultural com dois milênios de história, marcada por um contraste entre o antigo e o moderno.",
         imagem: "images/cards/londres.jpg",
+        link: "londres.html"
     }
-    
-]
+];
 
-            function montarCards() {
-            const container = document.getElementById("cards-container");
+function montarCards() {
+    const container = document.getElementById("cards-container");
 
-            destinos.forEach(destino => {
-                const card = document.createElement("div");
-                card.classList.add("card");
+    destinos.forEach(destino => {
+        const card = document.createElement("div");
+        card.classList.add("card");
 
-                card.innerHTML = `
-                <img src="${destino.imagem}" alt="${destino.nome}">
-                <h2>${destino.nome}</h2>
-                <p>${destino.pais}</p>
-                <p>${destino.descricao}</p>
-                `;
+        // conteúdo visual do card
+        card.innerHTML = `
+            <img src="${destino.imagem}" alt="${destino.nome}">
+            <h2>${destino.nome}</h2>
+            <p>${destino.pais}</p>
+            <p>${destino.descricao}</p>
+        `;
 
-                container.appendChild(card);
-            });
-            }
+        // evento de clique q redireciona para a página de detalhes
+        card.addEventListener("click", () => {
+            window.location.href = destino.link;
+        });
 
-            document.addEventListener("DOMContentLoaded", montarCards);
+        container.appendChild(card);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", montarCards);
